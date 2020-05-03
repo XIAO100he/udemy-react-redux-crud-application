feb9b0858a5705fdad13d745cfa380cf9d3baf70
XIAO100he <xiao100he@gmil.com>
2020-05-03 12:13:03 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 //ミドルウェア。action createrがactionの代わりに関数を返すことができる。非同期処理が可能に
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 // import serviceWorker  from 'serviceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -19,7 +21,12 @@ ReactDOM.render(
   // 既存のコンポーネント(App)をPrividerコンポーネントでラップし、store属性に定数storeを渡す
   // これによりアプリ内の全コンポーネントでstoreを使うことができる（親→子、子→孫　とする必要がない）
   <Provider store={ store }>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
