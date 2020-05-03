c10121ebfa0ae3b71914a879e6553112b15ccfbf
XIAO100he <xiao100he@gmil.com>
2020-05-03 15:17:04 +0900

    implement delete feature

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import { Provider } from 'react-redux';
 //ミドルウェア。action createrがactionの代わりに関数を返すことができる。非同期処理が可能に
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 // import serviceWorker  from 'serviceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+//dev環境の時だけ利用するようにする
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   // 既存のコンポーネント(App)をPrividerコンポーネントでラップし、store属性に定数storeを渡す
@@ -23,8 +28,10 @@ ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
-        <Route exact path="/" component={EventsIndex} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route path="/" component={EventsIndex} />
+        <Route path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
